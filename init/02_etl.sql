1d0edef9b4796b4f686099dba8c8064998d5e984
Dell <yegvenya@gmail.com>
2026-03-15 10:55:08 +0400

    update

--- init/02_etl.sql
+++ init/02_etl.sql
@@ -1,25 +1,25 @@
 -- COPY must read files inside the container; we mounted ./data to /docker-entrypoint-initdb.d/data
 \echo 'Loading employees...'
 COPY employees(employee_id,first_name,last_name,email,salary)
-FROM '/docker-entrypoint-initdb.d/data/employees.csv'
+FROM '/docker-entrypoint-initdb.d/data/public_schema/employees.csv'
 WITH (FORMAT csv, HEADER true);
 
 \echo 'Loading customers...'
 COPY customers(customer_id,customer_name,address,city,zip_code)
-FROM '/docker-entrypoint-initdb.d/data/customers.csv'
+FROM '/docker-entrypoint-initdb.d/data/public_schema/customers.csv'
 WITH (FORMAT csv, HEADER true);
 
 \echo 'Loading products...'
 COPY products(product_id,product_name,price,description,category)
-FROM '/docker-entrypoint-initdb.d/data/products.csv'
+FROM '/docker-entrypoint-initdb.d/data/public_schema/products.csv'
 WITH (FORMAT csv, HEADER true);
 
 \echo 'Loading orders...'
 COPY orders(order_id,order_date,year,quarter,month)
-FROM '/docker-entrypoint-initdb.d/data/orders.csv'
+FROM '/docker-entrypoint-initdb.d/data/public_schema/orders.csv'
 WITH (FORMAT csv, HEADER true);
 
 \echo 'Loading sales...'
 COPY sales(transaction_id,order_id,product_id,customer_id,employee_id,total_sales,quantity,discount)
-FROM '/docker-entrypoint-initdb.d/data/sales.csv'
+FROM '/docker-entrypoint-initdb.d/data/public_schema/sales.csv'
 WITH (FORMAT csv, HEADER true);
